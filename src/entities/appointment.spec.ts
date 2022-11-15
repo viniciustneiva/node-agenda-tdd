786615664a8d918305618fd74675500283c2dc16
Vinicius Neiva <viniciustneiva@gmail.com>
2022-11-15 11:03:39 -0300

    feat: added a function to handle date more efficiently

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -1,11 +1,11 @@
 import { expect, test } from 'vitest'
+import { getFutureDate } from '../tests/utils/get-future-date'
 import { Appointment } from './appointment'
 
 test('create an appointment', () => {
-    const startsAt = new Date()
-    const endsAt = new Date()
-    startsAt.setDate(startsAt.getDate() +1)
-    endsAt.setDate(endsAt.getDate() +2)
+    const startsAt = getFutureDate('2022-11-15')
+    const endsAt = getFutureDate('2022-11-16')
+
     const appointment = new Appointment({
         customer: "John Doe",
         startsAt,
@@ -17,10 +17,8 @@ test('create an appointment', () => {
 })
 
 test('cannot create an appointment when the end date is sooner than start date', () => {
-    const startsAt = new Date()
-    const endsAt = new Date()
-    startsAt.setDate(startsAt.getDate() +2)
-    endsAt.setDate(endsAt.getDate() +1)
+    const startsAt = getFutureDate('2022-11-15')
+    const endsAt = getFutureDate('2022-11-14')
     
 
     expect(() => {
--- src/use-cases/create-appointment.spec.ts
+++ src/use-cases/create-appointment.spec.ts
@@ -1,15 +1,14 @@
 import { describe, expect, it } from "vitest"
 import { Appointment } from "../entities/appointment";
+import { getFutureDate } from "../tests/utils/get-future-date";
 import { CreateAppointment } from "./create-appointment"
 
 describe('Create Appointment', () => {
     it('should be able to create an appointment', () => {
         const createAppointment = new CreateAppointment();
-        const startsAt = new Date()
-        const endsAt = new Date()
+        const startsAt = getFutureDate('2022-11-14')
+        const endsAt = getFutureDate('2022-11-16')
     
-        endsAt.setDate(endsAt.getDate() +2)
-        startsAt.setDate(startsAt.getDate() +1)
         expect(createAppointment.execute({
             customer: 'John Doe',
             startsAt,
