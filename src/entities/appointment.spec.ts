f79a021256e2d561239c4d2c151bbe078d69400b
Vinicius Neiva <viniciustneiva@gmail.com>
2022-11-15 10:32:29 -0300

    creating new test for datetime issues

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -2,12 +2,31 @@ import { expect, test } from 'vitest'
 import { Appointment } from './appointment'
 
 test('create an appointment', () => {
+    const startsAt = new Date()
+    const endsAt = new Date()
+
+    endsAt.setDate(endsAt.getDate() +1)
     const appointment = new Appointment({
         customer: "John Doe",
-        startsAt: new Date(),
-        endsAt: new Date()
+        startsAt,
+        endsAt
     })
 
     expect(appointment).toBeInstanceOf(Appointment)
     expect(appointment.customer).toEqual("John Doe")
+})
+
+test('cannot create an appointment when the end date is sooner than start date', () => {
+    const startsAt = new Date()
+    const endsAt = new Date()
+
+    endsAt.setDate(endsAt.getDate() -1)
+
+    expect(() => {
+        return new Appointment({
+            customer: "John Doe",
+            startsAt,
+            endsAt
+        })
+    }).toThrowError()
 })
